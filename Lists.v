b40d356618f5fa083760a6c7588042c35e5563c5
elton-cs <govada.elton@gmail.com>
2023-10-11 11:25:07 -0500

    Solve advanced (alternate)

--- Lists.v
+++ Lists.v
@@ -366,25 +366,39 @@ Qed.
     lists at the same time with the "multiple pattern" syntax we've
     seen before. *)
 
-Fixpoint alternate (l1 l2 : natlist) : natlist
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+Fixpoint alternate (l1 l2 : natlist) : natlist :=
+  match l1 , l2 with
+  | nil , _ => l2
+  | _ , nil => l1
+  | h1 :: t1 , h2 :: t2 => h1 :: h2 :: (alternate t1 t2)
+  end.
+
 
 Example test_alternate1:
   alternate [1;2;3] [4;5;6] = [1;4;2;5;3;6].
-  (* FILL IN HERE *) Admitted.
+Proof.
+  intros. simpl. reflexivity.
+Qed.
 
 Example test_alternate2:
   alternate [1] [4;5;6] = [1;4;5;6].
-  (* FILL IN HERE *) Admitted.
+Proof.
+  intros. simpl. reflexivity.
+Qed.
 
 Example test_alternate3:
   alternate [1;2;3] [4] = [1;4;2;3].
-  (* FILL IN HERE *) Admitted.
+Proof.
+  intros. simpl. reflexivity.
+Qed.
 
 Example test_alternate4:
   alternate [] [20;30] = [20;30].
-  (* FILL IN HERE *) Admitted.
-(** [] *)
+Proof.
+  intros. simpl. reflexivity.
+Qed.
+
+  (** [] *)
 
 (* ----------------------------------------------------------------- *)
 (** *** Bags via Lists *)
