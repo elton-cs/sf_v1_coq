7b0ff8089ba65df4246c30ad1fca2fe9b734915e
elton-cs <govada.elton@gmail.com>
2023-10-12 13:05:31 -0500

    Solve standard (more_poly_exercises)

--- Poly.v
+++ Poly.v
@@ -419,12 +419,20 @@ Qed.
 Theorem rev_app_distr: forall X (l1 l2 : list X),
   rev (l1 ++ l2) = rev l2 ++ rev l1.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros.
+  induction l1 as [| h t IHl1].
+  - simpl. rewrite app_nil_r. reflexivity.
+  - simpl. rewrite IHl1. rewrite app_assoc. reflexivity.
+Qed.  
 
 Theorem rev_involutive : forall X : Type, forall l : list X,
   rev (rev l) = l.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros.
+  induction l as [| h t IHl].
+  - simpl. reflexivity.
+  - simpl. rewrite rev_app_distr. rewrite IHl. simpl. reflexivity.
+Qed.
 (** [] *)
 
 (* ================================================================= *)
