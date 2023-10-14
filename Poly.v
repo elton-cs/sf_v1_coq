cee9bd3a32f12d2ba889643988f50cd1bf15ee6f
elton-cs <govada.elton@gmail.com>
2023-10-13 20:19:39 -0500

    Solve especially useful (flat_map)

--- Poly.v
+++ Poly.v
@@ -829,12 +829,15 @@ Qed.
 
 Fixpoint flat_map {X Y: Type} (f: X -> list Y) (l: list X)
                    : list Y
-  (* REPLACE THIS LINE WITH ":= _your_definition_ ." *). Admitted.
+:= match l with
+   | [] => []
+   | h :: t => app (f h) (flat_map f t)  
+   end. 
 
 Example test_flat_map1:
   flat_map (fun n => [n;n;n]) [1;5;4]
   = [1; 1; 1; 5; 5; 5; 4; 4; 4].
- (* FILL IN HERE *) Admitted.
+Proof. simpl. reflexivity. Qed.
 (** [] *)
 
 (** Lists are not the only inductive type for which [map] makes sense.
